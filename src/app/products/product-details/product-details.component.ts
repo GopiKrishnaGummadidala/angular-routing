73ec7d179aabf96a277cfdf576f26d6e7d2f0365
Gopi <gopikrishna.gummadidala@gmail.com>
2021-04-10 22:06:52 +0200

    product details component is added with rotuing

--- src/app/products/product-details/product-details.component.ts
+++ src/app/products/product-details/product-details.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Product } from "../product";
+import { ProductService } from "../product.service";
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  selector: "app-product-details",
+  templateUrl: "./product-details.component.html",
+  styleUrls: ["./product-details.component.scss"],
 })
 export class ProductDetailsComponent implements OnInit {
+  pageTitle = "Product Detail";
+  product: Product;
+  errorMessage: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
+  ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get("id");
+    this.getProduct(id);
   }
 
+  getProduct(id: number) {
+    this.productService.getProduct(id).subscribe({
+      next: (product) => this.onProductRetrieved(product),
+      error: (err) => (this.errorMessage = err),
+    });
+  }
+
+  onProductRetrieved(product: Product): void {
+    this.product = product;
+
+    if (this.product) {
+      this.pageTitle = `Product Detail: ${this.product.productName}`;
+    } else {
+      this.pageTitle = "No product found";
+    }
+  }
 }
