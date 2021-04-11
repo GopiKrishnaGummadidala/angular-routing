69bcadce42c93a630f66d85ebf9c5c30212a7361
Gopi <gopikrishna.gummadidala@gmail.com>
2021-04-11 16:42:57 +0200

    route navigation events added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from "@angular/core";
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+} from "@angular/router";
 import { slideInAnimation } from "./app.animation";
 
 @Component({
@@ -9,4 +16,42 @@ import { slideInAnimation } from "./app.animation";
 })
 export class AppComponent {
   title = "angular-routing";
+  pageTitle = "Product Management";
+  loading = true;
+
+  // get isLoggedIn(): boolean {
+  //   return this.authService.isLoggedIn;
+  // }
+
+  // get userName(): string {
+  //   if (this.authService.currentUser) {
+  //     return this.authService.currentUser.userName;
+  //   }
+  //   return "";
+  // }
+
+  constructor(private router: Router) {
+    router.events.subscribe((routerEvent: any) => {
+      this.checkRouterEvent(routerEvent);
+    });
+  }
+
+  checkRouterEvent(routerEvent: Event): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
+
+    if (
+      routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError
+    ) {
+      this.loading = false;
+    }
+  }
+
+  logOut(): void {
+    // this.authService.logout();
+    // this.router.navigateByUrl("/welcome");
+  }
 }
