4e677536e9a99f9cbc0d1be5ce229f9fdd27f4ab
Gopi <gopikrishna.gummadidala@gmail.com>
2021-04-11 13:07:27 +0200

    UI added for Info and tag components

--- src/app/products/product-edit/product-info/product-info.component.ts
+++ src/app/products/product-edit/product-info/product-info.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { ActivatedRoute } from "@angular/router";
+import { Product } from "../../product";
 
 @Component({
-  selector: 'app-product-info',
-  templateUrl: './product-info.component.html',
-  styleUrls: ['./product-info.component.scss']
+  selector: "app-product-info",
+  templateUrl: "./product-info.component.html",
+  styleUrls: ["./product-info.component.scss"],
 })
 export class ProductInfoComponent implements OnInit {
+  @ViewChild(NgForm, { static: false }) productForm: NgForm;
 
-  constructor() { }
+  errorMessage: string;
+  product: Product;
 
-  ngOnInit() {
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.parent.data.subscribe((data) => {
+      if (this.productForm) {
+        this.productForm.reset();
+      }
+
+      this.product = data["resolvedData"].product;
+    });
   }
-
 }
--- src/app/products/product-edit/product-tag/product-tag.component.ts
+++ src/app/products/product-edit/product-tag/product-tag.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Product } from "../../product";
 
 @Component({
-  selector: 'app-product-tag',
-  templateUrl: './product-tag.component.html',
-  styleUrls: ['./product-tag.component.scss']
+  selector: "app-product-tag",
+  templateUrl: "./product-tag.component.html",
+  styleUrls: ["./product-tag.component.scss"],
 })
 export class ProductTagComponent implements OnInit {
+  errorMessage: string;
+  newTags = "";
+  product: Product;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.route.parent.data.subscribe((data) => {
+      this.product = data["resolvedData"].product;
+    });
   }
 
+  // Add the defined tags
+  addTags(): void {
+    if (!this.newTags) {
+      this.errorMessage =
+        "Enter the search keywords separated by commas and then press Add";
+    } else {
+      const tagArray = this.newTags.split(",");
+      this.product.tags = this.product.tags
+        ? this.product.tags.concat(tagArray)
+        : tagArray;
+      this.newTags = "";
+      this.errorMessage = "";
+    }
+  }
+
+  // Remove the tag from the array of tags.
+  removeTag(idx: number): void {
+    this.product.tags.splice(idx, 1);
+  }
 }
