fc717d23756d50034faef7332be27fe617f515ca
Gopi <gopikrishna.gummadidala@gmail.com>
2021-04-12 20:47:43 +0200

    secondary routes

--- src/app/messages/message/message.component.ts
+++ src/app/messages/message/message.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { MessageService } from "../message.service";
 
 @Component({
-  selector: 'app-message',
-  templateUrl: './message.component.html',
-  styleUrls: ['./message.component.scss']
+  selector: "app-message",
+  templateUrl: "./message.component.html",
+  styleUrls: ["./message.component.scss"],
 })
-export class MessageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class MessageComponent {
+  get messages() {
+    return this.messageService.messages;
   }
 
+  constructor(private messageService: MessageService, private router: Router) {}
+
+  close(): void {
+    // Close the popup.
+    this.router.navigate([{ outlets: { popup: null } }]);
+    this.messageService.isDisplayed = false;
+  }
 }
